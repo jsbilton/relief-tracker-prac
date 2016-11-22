fd06f0c0ea9c9c8ada9f0e0245108eb5bf19ef60
Jared Bilton <jsbilton@gmail.com>
2016-11-21 22:32:03 -0500

    step 17 remove location

--- web/src/pages/locations/show.js
+++ web/src/pages/locations/show.js
@@ -1,11 +1,12 @@
 const React = require('react')
 const xhr = require('xhr')
-const { Link } = require('react-router')
+const { Link, Redirect } = require('react-router')
 
 const Location = React.createClass({
   getInitialState() {
     return {
-      location: {}
+      location: {},
+      removed: false
     }
   },
   componentDidMount() {
@@ -16,11 +17,24 @@ const Location = React.createClass({
       this.setState({ location })
     })
   },
+  handleRemove(e) {
+    e.preventDefault()
+    if (confirm('Removing Item, You sure?') ) {
+      xhr.del('http://localhost:4000/locations/' + this.state.location.id, { json: this.state.location}, (e, r, b) => {
+        if (e) return console.log(e.message)
+        this.setState({ removed: true})
+      })
+    }
+  },
   render() {
     return (
       <div>
+      { this.state.removed ? <Redirect to="/locations" /> : null}
         <h3>{ this.state.location.name }</h3>
         <Link to={`/locations/${this.state.location.id}/edit`}>Edit Location</Link>
+        |
+        <a href='#'
+          onClick={this.handleRemove}>Remove Location</a>
         |
         <Link to="/locations">Return</Link>
       </div>
